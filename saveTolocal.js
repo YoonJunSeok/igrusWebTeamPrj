d49f47cb7ef19398763d3a061d184f03d54c9066
JunSeok Yoon <74297322+YoonJunSeok@users.noreply.github.com>
2021-05-21 14:14:33 +0900

    Example code for saving

--- saveTolocal.js
+++ saveTolocal.js
@@ -1,45 +1,44 @@
+// 저장한 것을 표로 나타내기 위한 예제.
+
 // var
 var studentData;
 var studentTable = document.getElementById("studentList");
 
 // save example
-var arr = [{name: '홍길동', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B', subject3: 'C', subject4: 'A', subject5: 'A', subject6: 'A'},
-            {name: '홍길은', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B', subject3: 'C', subject4: 'A', subject5: 'A', subject6: 'A'},
-            {name: '홍길금', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B', subject3: 'C', subject4: 'A', subject5: 'A', subject6: 'A'},
-            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B', subject3: 'C', subject4: 'A', subject5: 'A', subject6: 'A'},
-            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B', subject3: 'C', subject4: 'A', subject5: 'A', subject6: 'A'},
-            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B', subject3: 'C', subject4: 'A', subject5: 'A', subject6: 'A'},
-            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B', subject3: 'C', subject4: 'A', subject5: 'A', subject6: 'A'},
+var arr = [
+            {name: '홍길동', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길은', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길금', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'},
+            {name: '홍길둥', schoolNum: '12121212', major: '컴퓨터공학과', subject1: 'A', subject2: 'B'}
 ]
 
 // save data to localStorage
 function saveDataToLocalStorage() {
     localStorage.setItem("students", JSON.stringify(arr));
-}
-
-// get data in localStorage
-function getDataFromLocalStorage() {
-    studentData = JSON.parse(localStorage.getItem("students"));
-}
-
-// print table
-function createTable(data) {
-
-    getDataFromLocalStorage();
-
-    for (key in studentData) {
-        var row = '<tr><td>' + studentData[key].name + '</td>' 
-        + '<td>' + studentData[key].schoolNum + '</td>'
-        + '<td>' + studentData[key].major + '</td>'
-        + '<td>' + studentData[key].subject1 + '</td>'
-        + '<td>' + studentData[key].subject2 + '</td>'
-        + '<td>' + studentData[key].subject3 + '</td>'
-        + '<td>' + studentData[key].subject4 + '</td>'
-        + '<td>' + studentData[key].subject5 + '</td>'
-        + '<td>' + studentData[key].subject6 + '</td>';
-
-        studentTable.innerHTML += row;
-    }
 }
 
 saveDataToLocalStorage();
