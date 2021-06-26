2b3754fc817ee169629e78825dd32b2dcd638802
youngseojang01 <83485151+youngseojang01@users.noreply.github.com>
2021-06-26 19:05:17 +0900

    Update studetnAddDo.js

--- 프로젝트_수정/studetnAddDo.js
+++ 프로젝트_수정/studetnAddDo.js
@@ -2,8 +2,8 @@ window.onload = function(){
     document.getElementById("studentform").onsubmit = addStudent
     document.getElementById("clearButton").onclick = clearStorage
     document.getElementById("removeButton").onclick = removeStudent
-    document.getElementById("retrieveButton").onclick = getStudent
-    document.getElementById("editButton").onclick = modifyStudent
+    //document.getElementById("retrieveButton").onclick = getStudent
+   // document.getElementById("editButton").onclick = modifyStudent
 }
 
 function getStudent(){}
@@ -50,73 +50,6 @@ function addStudent(){
 }
 
 function modifyStudent(){
-    document.writeln(
-       /* <table border="1" class="table table-bordered">
-                <tr>
-                    <th><label for="stdNum">학번</label></th> 
-                    <td><input type="text" id="stdNum" name="stdNum" required></input></td>
-                </tr>
-                <tr>
-                    <th><label for="stdName">이름</label></th>
-                    <td><input type="text" id="stdName" name="stdName" required></input></td>
-                </tr>
-                <tr>
-                    <th><label for="stdGender">성별</label></th>
-                    <td>
-                    <label><input type="radio" id="stdGender" name="stdGender"value="male">남</input></label>
-                    <label><input type="radio" name="stdGender"value="female">여</input></label>
-                    </td>
-                </tr>
-                <tr>
-                    <th><label for="stdBirth">생년월일</label></th>
-                    <td><input type="date" id="stdBirth" name="stdBirth" required></input></td>
-                </tr>
-                <tr>
-                    <th><label for="stdMajor">전공</label></th>
-                    <td><input type="text" id="stdMajor" name="stdMajor" required></input></td>
-                </tr>
-                <tr>
-                    <th><label for="subject1">과목1</label></th>
-                    <td><select type="text" id="subject1" name="subject1">
-                        <option value="4.5" selected>A+</option>
-                        <option value="4.0" >A0</option>
-                        <option value="3.5">B+</option>
-                        <option value="3.0">B0</option>
-                        <option value="2.5">C+</option>
-                        <option value="2.0">C0</option>
-                    </select></td>
-                </tr>
-                <tr>
-                    <th><label for="subject2">과목2</label></th>
-                    <td><select type="text" id="subject2" name="subject2">
-                        <option value="4.5" selected>A+</option>
-                        <option value="4.0" >A0</option>
-                        <option value="3.5">B+</option>
-                        <option value="3.0">B0</option>
-                        <option value="2.5">C+</option>
-                        <option value="2.0">C0</option>
-                    </select></td>
-                </tr>
-                <tr>
-                    <th><label for="subject3">과목3</label></th>
-                    <td><select type="text" id="subject3" name="subject3">
-                        <option value="4.5" selected>A+</option>
-                        <option value="4.0" >A0</option>
-                        <option value="3.5">B+</option>
-                        <option value="3.0">B0</option>
-                        <option value="2.5">C+</option>
-                        <option value="2.0">C0</option>
-                    </select></td>
-                </tr>
-                <tr>
-                    <td colspan="2" style="text-align: center;">
-                    <button type="submit" class="btn btn-primary btn-sm">저장</button>
-                    <button type="reset" class="btn btn-primary btn-sm">초기화</button>
-                    </td>
-                </tr>
-            </table>
-*/
-    )
 }
 
 function removeStudent(){
