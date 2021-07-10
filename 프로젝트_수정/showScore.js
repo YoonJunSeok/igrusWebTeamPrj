7de533c0c9825bf82229bb81326afcd7a63ed371
youngseojang01 <83485151+youngseojang01@users.noreply.github.com>
2021-07-10 17:38:22 +0900

    Add files via upload
    성적 확인 페이지 추가

---
+++ 프로젝트_수정/showScore.js
@@ -0,0 +1,30 @@
+window.onload = function(){
+  document.getElementById("showButton").onsubmit = show()
+}
+
+
+function show(){
+  // localstorage에서 student arr 가져오기
+  studentArr = JSON.parse(localStorage.getItem("students"));
+  // 확인할 학번
+  stdNum = document.getElementById("schoolNum").value;
+
+  // 확인
+  if(stdNum==""){
+        alert("학번을 입력하세요.");
+  }  else {
+  var list = '';
+  for(let i=0; i< studentArr.length; i++){
+      if (studentArr[i].schoolNum === stdNum) {
+          list += studentArr[i];
+      }
+  }
+  if (list === ''){
+    list += stdNum + '학번을 가진 학생이 없습니다.';
+    alert(list);
+  }
+  else {
+    document.getElementById("result").innerText = list;
+  }
+  }
+}
