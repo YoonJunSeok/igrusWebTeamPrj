4619bcfd7bba91d634a8016482d8ebab6f92ff50
youngseojang01 <83485151+youngseojang01@users.noreply.github.com>
2021-07-17 15:35:05 +0900

    Update studentinfo.js
    axios 수정

--- LoginSystem/js/studentinfo.js
+++ LoginSystem/js/studentinfo.js
@@ -1,68 +1,74 @@
-function store() { // localStorage에 데이터 저장
-    var num = document.getElementById('stdNum').value; //학번을 localstorage의 key값으로 저장한다.
-    var name = document.getElementById('stdName').value; 
-    var gender = "";
-	for (var i=0;i<obj;i++) {
-		if (document.getElementsByName('stdGender')[i].checked==true){
-			gender+=document.getElementsByName('stdGender')[i].value;
-		}
-	}
-    var date = document.getElementById('stdBirth').value;
-    var adress = document.getElementById('stdAdress').value;
-    var phone = document.getElementById('stdPhone').value;
-
-    const Student = {
-        name : name,
-        gender: gender, 
-        date: date, 
-        phone: phone, 
-        adress: adress,
-    }
-
-    window.localStorage.setItem(num, JSON.stringify(Student)); 
-    // Student객체를 string으로 저장
-}
-
-function retrieveRecords() { // 학생 정보 조회
-    // localStorage에 있는 키값에 해당하는 데이터 불러오기
-    console.log("retrieve records");
-    var key = document.getElementById('retrieveNum').value;
-    if(key==""){
-        alert("학번을 입력하세요.");
-    }
-    var records = window.localStorage.getItem(key);
-
-    var paragraph = document.createElement("div"); // create a new div element
-    var infor = document.createTextNode(records); // give it records
-    paragraph.appendChild(infor); // add the text node to the newly created div
-
-    var currentDiv = document.getElementById('retrieve'); 
-    document.body.insertBefore(paragraph, currentDiv);
-}
-
-function editRecords() { // 학생 정보 수정
-    
-}
-
-function removeItem(){
-    // 키값에 해당하는 데이터 삭제
-    var key = document.getElementById('removeNum').value;
-    localStorage.removeItem(key);
-    alert("정보를 삭제하시겠습니까?");
-    console.log(key, "의 정보를 삭제합니다.");
-}
-
-function clearStorage(){
-    // localStorage에 있는 모든 데이터 삭제
-    alert("전체 정보를 삭제하시겠습니까?");
-    localStorage.clear();
-    console.log("clear records");
-}
-
 window.onload = function(){
-    document.getElementById("studentform").onsubmit = store
+    document.getElementById("studentform").onsubmit = addStudent
     document.getElementById("clearButton").onclick = clearStorage
-    document.getElementById("removeButton").onclick = removeItem
-    document.getElementById("retrieveButton").onclick = retrieveRecords
-    document.getElementById("editButton").onclick = editRecords
+    document.getElementById("removeButton").onclick = removeStudent
+}
+
+import axios from 'axios';
+
+// db url
+const config = {
+  DBUrl: "mongodb+srv://admin:16team@cluster0.hhvfw.mongodb.net/users?retryWrites=true&w=majority"
+}
+
+// 함수
+
+function addStudent() {
+  // studentAdd.html에서 입력받은 data 불러오기
+  var num = document.getElementById('stdNum').value;      // 학번 schoolNum
+  var name = document.getElementById('stdName').value;    // 이름 name
+  var obj = document.getElementsByName('stdGender').length;
+  var gender = "";                                        // 성별 gender
+  for (var i=0;i<obj;i++) {
+    if (document.getElementsByName('stdGender')[i].checked==true){
+      gender+=document.getElementsByName('stdGender')[i].value;
+  }}
+  var date = document.getElementById('stdBirth').value;   // 생년월일 date
+  var major = document.getElementById('stdMajor').value;  // 전공 major
+  var s1 = document.getElementById('subject1');  // 성적1
+  var subject1 = s1.options[s1.selectedIndex].value;
+  var s2 = document.getElementById('subject2'); // 성적2
+  var subject2 = s2.options[s2.selectedIndex].value;
+  var s3 = document.getElementById('subject3'); // 성적3
+  var subject3 = s3.options[s3.selectedIndex].value;
+
+  // POST 요청 (새로운 리소스를 create하여 서버에 업로드) 
+  axios.post(`${config.DBUrl}/`, {
+      name : name,
+      schoolNum : num,
+      gender: gender, 
+      date : date, 
+      major : major,
+      subject1 : subject1,
+      subject2 : subject2,
+      subject3 : subject3
+    })
+    .then(function (response) {
+    console.log(response);
+    })
+    .catch(function (error) {
+    console.log(error);
+    });
+}
+
+function removeStudent() {
+  // 삭제할 학번
+  stdNum = document.getElementById('removeNum').value; 
+  if(stdNum==""){
+      alert("학번을 입력하세요.");
+  }
+  else {
+  alert("정보를 삭제하시겠습니까?");
+  console.log(stdNum, "의 정보를 삭제합니다.");
+
+  // db에서 삭제
+  axios.delete(`${config.DBUrl}/`, {
+    data: {
+      schoolNum: stdNum
+    },
+    withCredentials: true;
+  })
+  .then((response) => response.data)
+  .catch((error)=>console.log(error));
+  
 }
