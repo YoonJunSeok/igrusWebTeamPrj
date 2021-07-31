db581918cda2734773d4e4a996a3371838b50f9f
YoonJunSeok <imjunseok0407@gmail.com>
2021-07-31 09:59:49 +0900

    error code in testAxios.js

--- server/js/testAxios.js
+++ server/js/testAxios.js
@@ -1,28 +1,21 @@
-// const axios = require('axios');
+const axios = require('axios');
 
-// axios({
-//     method: 'get',
-//     url: 'http://localhost:3000/test',
-//     data: {
-//         id: 'saebeok',
-//     },
-//     headers: {
-//         //Authorization: 'Bearer ', // + varToken
-//     }
-// }).then((res) => {
-//     const id = document.getElementById('test');
-//     id = res.id;
-//     console.log(id);
-// }).then((err) => {
-//     console.log('err');
-// })
-
-const button = document.querySelector("#dog")
-const url = 'https://dog.ceo/api/breeds/image/random'
-
-const getId = () => {
-    axios.get(url).then((res) => {
-        console.log(res);
-    })
-}
-button.addEventListener("click", getDogImages)
+// error code
+// ailed to load resource: the server responded with a status of 404 (Not Found)
+axios({
+    method: 'get',
+    url: 'http://localhost:3000/test',
+    data: {
+        id: 'good',
+    },
+    headers: {
+        //Authorization: 'Bearer ', // + varToken
+        // how get varToken?
+    }
+}).then((res) => {
+    const id = document.getElementById('test');
+    id = res.id;
+    console.log(id);
+}).then((err) => {
+    console.log('err');
+})
